f0debad418be681b4b32f5d5dd5db5512ca8625a
Cristian Mello <cristianc.mello@gmail.com>
2024-08-21 05:05:55 -0300

    feat: removing init impl from walking skeleton

--- src/test/java/ccmello/simplestore/e2e/Hooks.java
+++ src/test/java/ccmello/simplestore/e2e/Hooks.java
@@ -1,17 +1,10 @@
 package ccmello.simplestore.e2e;
 
-import ccmello.simplestore.ProductRepository;
 import io.cucumber.java.After;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class Hooks {
-    @Autowired
-    private ProductRepository repo;
-
     @After
-    public void cleanUp() {
-        repo.deleteAll();
-    }
+    public void cleanUp() {}
 
     // Cucumber supported hooks
     // @Before     => public void init()
--- src/test/java/ccmello/simplestore/e2e/PreparacaoDaHistoria.java
+++ src/test/java/ccmello/simplestore/e2e/PreparacaoDaHistoria.java
@@ -1,37 +1,10 @@
 package ccmello.simplestore.e2e;
 
-import ccmello.simplestore.Product;
-import ccmello.simplestore.ProductRepository;
 import io.cucumber.java.pt.Dados;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 public class PreparacaoDaHistoria {
-    @Autowired
-    private ProductRepository repo;
-
     @Dados("produtos cadastrados na loja")
     public void produtosCadastradosNaLoja() {
-        var first = new Product();
-        var second = new Product();
-        var third = new Product();
 
-        first.setCategory("Utensilios");
-        first.setName("Kit 12 Facas");
-        first.setQuantity(3);
-        first.setAvailable(true);
-
-        second.setCategory("Utensilios");
-        second.setName("Colher de Silicone");
-        second.setQuantity(12);
-        second.setAvailable(true);
-
-        third.setCategory("Utensilios");
-        third.setName("Ralador 4 faces Inox");
-        third.setQuantity(0);
-        third.setAvailable(false);
-
-        repo.saveAll(List.of(first, second, third));
     }
 }
